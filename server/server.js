bae3d281c7d478bed2a0e3e646cbba6bb9931a98
Ciro Paredes <c23paredes@gmail.com>
2025-06-13 15:41:38 -0500

    fix: Corregir configuracion de deployment para Koyeb

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 const CLIENT_BUILD = process.env.CLIENT_BUILD || 'client/dist';
 
 app.use(cors());
